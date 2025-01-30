cd32b745ef71b198970c020453855b8214156b83
SharmaSheetal <sharmasheetal9798@gmail.com>
2025-01-29 18:51:06 -0700

    adding comments

--- chocolate_distribution.py
+++ chocolate_distribution.py
@@ -1,3 +1,14 @@
+"""
+Given an array arr[] of positive integers, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets among m students such that -
+      i. Each student gets exactly one packet.
+     ii. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum and return that minimum possible difference.
+
+Examples:
+
+Input: arr = [3, 4, 1, 9, 56, 7, 9, 12], m = 5
+Output: 6
+Explanation: The minimum difference between maximum chocolates and minimum chocolates is 9 - 3 = 6 by choosing following m packets :[3, 4, 9, 7, 9].
+"""
 def findMinDiff(self, arr, M):
     # Step 1: Sort the input array.
     # Sorting ensures that elements within any subarray of size M 
